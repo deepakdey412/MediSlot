37e704918b9208c71e82ba44ffff0d6ea396e9d9
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-09-17 21:51:04 +0530

    Update Service

--- MediSlot-bakend/src/main/java/com/DeepakDey/MediSlot_bakend/service/AuthService.java
+++ MediSlot-bakend/src/main/java/com/DeepakDey/MediSlot_bakend/service/AuthService.java
@@ -1,16 +1,35 @@
 package com.DeepakDey.MediSlot_bakend.service;
 
+import com.DeepakDey.MediSlot_bakend.dto.RegisterRequest;
+import com.DeepakDey.MediSlot_bakend.entity.Role;
+import com.DeepakDey.MediSlot_bakend.entity.User;
 import com.DeepakDey.MediSlot_bakend.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 @Service
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService(UserRepository userRepository) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public UserDto
+    public User register(RegisterRequest request) {
+        if(userRepository.existsByContact(request.getContact())) {
+            throw new RuntimeException("User already exists");
+        }
+
+        User user = User.builder()
+                .name(request.getName())
+                .email(request.getContact())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.valueOf(request.getRole().toUpperCase()))
+                .verified(false)
+                .build();
+
+        return  userRepository.save(user);
+    }
 }
